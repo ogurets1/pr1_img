c2a670f33bc5a1bdf0e6c3f8bb41e413786aa8e3
ogurets1 <120803416+ogurets1@users.noreply.github.com>
2024-04-12 12:57:23 +0400

    ogurets
    ааа

--- NeuroImageGen/test.js
+++
@@ -1,9 +0,0 @@
-import unirest from 'unirest';
-
-const api_key = 'sk-7eGUVOPkHrom4Oa7MQ0ULTlBBmEe2JhSPslPqtY8GyG2Hh524aE2vraSFiP4';
-
-import {Configuration,OpenAIApi} from 'openai';
-
-const openai = new OpenAI({
-  apiKey: con
-});
